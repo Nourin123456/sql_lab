85f2bbe57768dfb3fd96f1a8fdbc2720bc0534bb
Naveena Nourin <naveenanourinkm@gmail.com>
2026-03-13 11:39:22 +0530

    Create prgrm5-3.sql

---
+++ prgrm5-3.sql
@@ -0,0 +1,45 @@
+===PROCEDURE===
+DELIMITER //
+
+CREATE PROCEDURE member_status(IN cid INT, OUT membership VARCHAR(20))
+BEGIN
+   DECLARE cred INT;
+
+   SELECT credits INTO cred
+   FROM customer
+   WHERE customer_id = cid;
+
+   IF cred > 5000 THEN
+      SET membership = 'platinum';
+   ELSEIF cred >= 1000 AND cred <= 5000 THEN
+      SET membership = 'gold';
+   ELSE
+      SET membership = 'silver';
+   END IF;
+
+END //
+
+DELIMITER ;
+
+===OUTPUT===
+DELIMITER //
+
+CREATE PROCEDURE member_status(IN cid INT, OUT membership VARCHAR(20))
+BEGIN
+   DECLARE cred INT;
+
+   SELECT credits INTO cred
+   FROM customer
+   WHERE customer_id = cid;
+
+   IF cred > 5000 THEN
+      SET membership = 'platinum';
+   ELSEIF cred >= 1000 AND cred <= 5000 THEN
+      SET membership = 'gold';
+   ELSE
+      SET membership = 'silver';
+   END IF;
+
+END //
+
+DELIMITER ;
